4f96c110f384b1820a521c63977191c468dc30be
ixnp <enprocopios@gmail.com>
2019-06-10 11:54:39 -0700

    adding dupwords

---
+++ javascript-algorithms/dupWords.js
@@ -0,0 +1,10 @@
+//given a string of words, determine if the string has any duplicate words. 
+// in: "cookie cat he's a pet for your tummy!"
+// out: false
+// in: "Cookie Cat! He's super duper yummy! Cookie Cat! He left his family behind! Cookie Caaaaat! Now available at Gurgens off Route 109!"
+// in: true
+
+//part two
+// remove all duplicate words from string
+// in: "Cookie Cat! He's super duper yummy! Cookie Cat! He left his family behind! Cookie Caaaaat! Now available at Gurgens off Route 109!"
+// out: "super duper yummy! left his family behind! Caaaaat! Now available at Gurgens off Route 109!"
---
+++ javascript-algorithms/rotateMatrix.js
@@ -0,0 +1,9 @@
+//Given a 2d array of equal length and height, rotate the matrix 90 degrees
+//source: Cracking the coding interview, Gayle Mcdowell, chapter 1
+//in: [[1,2,3],
+//     [4,5,6],
+//     [7,8,9]]
+//
+//out: [[7,4,1],
+//      [8,5,2],
+//      [9,6,3]]
