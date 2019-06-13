e7119d4ea4197e35b25bc2547864b50f172b054a
ixnp <enprocopios@gmail.com>
2019-06-12 17:23:55 -0700

    added dupWords js solution

--- software-engineering/javascript-algorithms/strings/dupWords.js
+++ software-engineering/javascript-algorithms/strings/dupWords.js
@@ -8,3 +8,22 @@
 // remove all duplicate words from string
 // in: "Cookie Cat! He's super duper yummy! Cookie Cat! He left his family behind! Cookie Caaaaat! Now available at Gurgens off Route 109!"
 // out: "super duper yummy! left his family behind! Caaaaat! Now available at Gurgens off Route 109!"
+
+//solution 1
+function dupWords(string){
+    let str = string.split(' ')
+    let keyMap = {}
+    let dups = false
+    for(let i = 0; i < str.length; i++){
+      if(!keyMap[str[i]]){
+        keyMap[str[i]] = true;
+      } else {
+        dups = true
+        break
+      }
+    }
+    return dups
+  }
+  
+  dupWords("Cookie cat") //false
+  dupWords("cat, cat, cat") //true
